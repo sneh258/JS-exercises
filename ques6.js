621e0f8ffa02328296f657b34fdf5826dd3414c1
Sneha <snehalall.sneha@gmail.com>
2023-01-14 16:26:58 +0530

    feat : add function to check last word

---
+++ ques6.js
@@ -0,0 +1,22 @@
+// Write a JavaScript function ( both regular and arrow function) 
+// which takes a string as parameter 
+// to test whether a string end with "Script"(case-sensitive). The string length must be greater or equal to 6.
+
+function endsWith(inputOne)
+{
+
+    return inputOne.endsWith("Script") ? "True" : "False"
+
+}
+
+console.log(endsWith("I am Learning JavaScript"))
+
+const ends_With=(inputOne)=>{
+
+    return inputOne.endsWith("Script") ? "True" : "False"
+
+}
+
+console.log(ends_With("I am Learning Java"))
+
+
