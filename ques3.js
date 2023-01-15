69d1e12507bcf4a0830c10bb35f5d9245eefc4af
Sneha <snehalall.sneha@gmail.com>
2023-01-15 17:05:43 +0530

    feat : add function to change the capitalization of letters

--- ques3.js
+++ ques3.js
@@ -2,31 +2,42 @@
 // to change the capitalization of all letters in a given string and returns the result and then print it.
 
 
-let inputOne="MarRy"
+let inputOne="MarRy HaD a LitTle LAmp"
+let inputTwo="SneHA LaLL iT is"
 
 function string_letter_conversion(inputOne)
 {
 
     
-
-    for(let index=0;index<inputOne.length;index++)
-    {
-
-        if(inputOne[index]==inputOne[index].toLocaleUpperCase())
-        {
-            inputOne[index]=inputOne[index].toLocaleLowerCase();
-            
+    let newStr = "";
+    for(let index = 0; index < inputOne.length; ++index) {
+        let char = inputOne.charAt(index);
+        if(char === char.toUpperCase()){
+            newStr += inputOne.charAt(index).toLowerCase();
+        } else {
+            newStr += inputOne.charAt(index).toUpperCase();
         }
-
-        else{
-            inputOne[index]=inputOne[index].toLocaleUpperCase();
-        }
-    } 
-    return inputOne;
+    }
+    return newStr;
 }
 
-console.log(string_letter_conversion(inputOne))
 
-//console.log(inputOne[0].toLocaleUpperCase())
 
-//console.log(inputOne.replace(inputOne[0],inputOne[0].toLocaleLowerCase()))
+const interhangeOfCases = (inputTwo) => {
+    let newStr = "";
+    for(let index = 0; index < inputTwo.length; ++index) {
+        let char = inputTwo.charAt(index);
+        if(char === char.toUpperCase()){
+            newStr += inputTwo.charAt(index).toLowerCase();
+        } else {
+            newStr += inputTwo.charAt(index).toUpperCase();
+        }
+    }
+    return newStr;
+}
+
+
+module.exports={string_letter_conversion,interhangeOfCases};
+
+// console.log(string_letter_conversion(inputOne))
+// console.log(interhangeOfCases(inputTwo))
