1985a9a09d7ed31e71d14cf38b2f9f8b6fc5a24e
Sneha <snehalall.sneha@gmail.com>
2023-01-15 17:09:01 +0530

    feat : add function to find longest string in an array

--- ques4.js
+++ ques4.js
@@ -1,48 +1,54 @@
 // Write a JavaScript function (both regular and arrow) 
 // which takes an array of string as parameter to find the longest string from a given array
 
-let inputOne=["we", "love", "code", "academy"]
+let inputOne=["mycodeacademy","my","code"]
 
-function longest_element_array(inputOne)
+function longest_element_in_array(inputOne)
 {
     let max=0
     let index=0
+    let store_index=index
     let length=inputOne.length
 
     while(length)
     {
         if(inputOne[index].length>max)
         {
-            max=index;
+            max=inputOne[index].length;
+            store_index=index;
         }
         index++;
         length--;
     }
 
-    return inputOne[max];
+    return inputOne[store_index];
 }
 
 
-const longestElementArray=(inputOne)=>
+const longestElementInArray=(inputOne)=>
 {
 
     let max=0
     let index=0
+    let store_index=index
     let length=inputOne.length
 
     while(length)
     {
         if(inputOne[index].length>max)
         {
-            max=index;
+            max=inputOne[index].length;
+            store_index=index;
         }
         index++;
         length--;
     }
 
-    return inputOne[max];
+    return inputOne[store_index];
 
 }
 
-console.log(longest_element_array(inputOne))
-console.log(longestElementArray(inputOne))
+module.exports = {longest_element_in_array, longestElementInArray};
+
+// console.log(longest_element_in_array(inputOne));
+// console.log(longestElementInArray(inputOne));
