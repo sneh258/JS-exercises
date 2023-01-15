6b1b4efc910a6de00a2f6cee8e7b51b4c3c1fd2e
Sneha <snehalall.sneha@gmail.com>
2023-01-15 17:17:30 +0530

    test: add test cases for file 7

---
+++ ques7.test.js
@@ -0,0 +1,40 @@
+const {concatenateStrings,concatenate_Strings}=require('./ques7')
+
+test('testCaseOne', () => {
+    expect(concatenateStrings("this","is")).toBe("hiss");
+  });
+
+test('testCaseTwo', () => {
+    expect(concatenateStrings("this","javaScript")).toBe("hisavaScript");
+    
+  });
+
+test('testCaseThree', () => {
+    expect(concatenateStrings("love", "javaScript")).toBe("oveavaScript");
+    
+  });
+
+test('testCaseFour', () => {
+    expect(concatenateStrings("Script","done")).toBe("criptone");
+    
+  });
+
+test('testCaseFive', () => {
+    expect(concatenate_Strings("well","done")).toBe("ellone");
+    
+  });
+
+test('testCaseSix', () => {
+    expect(concatenate_Strings("a", "class")).toBe("lass");
+    
+  });
+
+test('testCaseSeven', () => {
+    expect(concatenate_Strings("my", "college")).toBe("yollege");
+    
+  });
+
+  test('testCaseEight', () => {
+    expect(concatenate_Strings("no", "preserved")).toBe("oreserved");
+    
+  });
