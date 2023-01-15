b2933c5f27ee10c1bcc5e37e9c427f8a8829b182
Sneha <snehalall.sneha@gmail.com>
2023-01-15 17:15:20 +0530

    test: add test cases for file 6

---
+++ ques6.test.js
@@ -0,0 +1,40 @@
+const {ends_With,endsWith}=require('./ques6')
+
+test('testCaseOne', () => {
+    expect(endsWith("i am learning javaScript")).toBe("True");
+  });
+
+test('testCaseTwo', () => {
+    expect(endsWith("this is javaScript")).toBe("True");
+    
+  });
+
+test('testCaseThree', () => {
+    expect(endsWith("i love javaScript")).toBe("True");
+    
+  });
+
+test('testCaseFour', () => {
+    expect(endsWith("Script")).toBe("True");
+    
+  });
+
+test('testCaseFive', () => {
+    expect(ends_With("this is well done")).toBe("False");
+    
+  });
+
+test('testCaseSix', () => {
+    expect(ends_With("a class")).toBe("False");
+    
+  });
+
+test('testCaseSeven', () => {
+    expect(ends_With("my college is kiit")).toBe("False");
+    
+  });
+
+  test('testCaseEight', () => {
+    expect(ends_With("no preserved")).toBe("False");
+    
+  });
